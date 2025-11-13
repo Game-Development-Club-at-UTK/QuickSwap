f1b78d403d4a75e13817c8c73713d3e978d39601
Cole Davis <152663614+bill3014@users.noreply.github.com>
2025-11-13 08:03:47 -0500

    Slide QOL Transition From Smash

--- Entities/Character/Player/ground_smash.gd
+++ Entities/Character/Player/ground_smash.gd
@@ -3,17 +3,24 @@ extends State
 
 @export var player : CharacterBody3D
 
+@onready var groundSmashSlideTTime : Timer = $Timer
+
 const groundSmashSpeed : float = -20.0
 
 func enter():
 	player.velocity.y = groundSmashSpeed
+	groundSmashSlideTTime.start()
+	
 
 func exit():
 	pass
 
 func update(_delta: float):
 	if player.is_on_floor():
-		transition.emit(self, "Idle")
+		if !groundSmashSlideTTime.is_stopped() && Input.is_action_pressed("slide"):
+			transition.emit(self, "Slide")
+		else:
+			transition.emit(self, "Idle")
 
 func physics_update(_delta: float):
 	player.move_and_slide()
